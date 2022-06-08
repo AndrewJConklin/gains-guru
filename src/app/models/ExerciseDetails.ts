30f029434e862d03f15dbbcab3d6751f000325cd
aconkli9 <aconkli9@ford.com>
2022-06-08 15:37:55 -0400

    Add image logic

--- src/app/models/ExerciseDetails.ts
+++ src/app/models/ExerciseDetails.ts
@@ -2,4 +2,11 @@ export type ExerciseDetails = {
   name: string,
   id: string,
   description: string,
+  images?: Image[]
+}
+
+type Image = {
+  id: string,
+  image: string,
+  is_main: boolean
 }
